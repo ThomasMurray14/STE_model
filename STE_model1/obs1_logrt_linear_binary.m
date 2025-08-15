c5dd01aa3218ad7bded4a6479d48c2e36c38ba14
ThomasMurray14 <41687196+ThomasMurray14@users.noreply.github.com>
2025-08-15 16:14:25 +0100

    update

---
+++ STE_model1/obs1_logrt_linear_binary.m
@@ -0,0 +1,113 @@
+function [logp, yhat, res] = obs1_logrt_linear_binary(r, infStates, ptrans)
+% [logp, yhat, res] = m1_logrt_linear_binary(r, infStates, ptrans)
+%
+% Calculates the log-probability of log-reaction times y (in units of
+% log-ms) according to the linear log-RT model developed for the SPIRL
+% study based on the Lawson logRT GLM.
+% (Designed to be compatible with the HGF Toolbox as part of TAPAS).
+%
+% INPUT
+%   r             struct      Struct obtained from tapas_fitModel.m fct
+%   infStates     tensor      Tensor containing inferred states from the
+%                             perceptual model    
+%   ptrans        vector      1xP vector with free param values (est space)
+%
+%   OPTIONAL:
+%
+% OUTPUT    
+%   logp          vector       1xN vector containing trialwise log
+%                              probabilities of logRTs
+%   yhat          vector       1xN vector containing noise-free predictions
+%   res           vector       1xN vector containing residuals
+%
+% _________________________________________________________________________
+% Author: Alex Hess
+%
+% Copyright (C) 2023 Translational Neuromodeling Unit
+%                    Institute for Biomedical Engineering
+%                    University of Zurich & ETH Zurich
+%
+% This file is released under the terms of the GNU General Public Licence
+% (GPL), version 3. You can redistribute it and/or modify it under the
+% terms of the GNU General Public License as published by the Free Software
+% Foundation, either version 3 of the License, or (at your option) any
+% later version.
+%
+% This file is distributed in the hope that it will be useful, but WITHOUT
+% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+% FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+% more details.
+% 
+% You should have received a copy of the GNU General Public License along
+% with this program. If not, see <https://www.gnu.org/licenses/>.
+% _________________________________________________________________________
+
+% Transform parameters to their native space
+be0  = ptrans(1);
+be1  = ptrans(2);
+be2  = ptrans(3);
+be3  = ptrans(4);
+be4  = ptrans(5);
+sa   = exp(ptrans(6));
+
+% Initialize returned log-probabilities, predictions,
+% and residuals as NaNs so that NaN is returned for all
+% irregualar trials
+n = size(infStates,1);
+logp = NaN(n,1);
+yhat = NaN(n,1);
+res  = NaN(n,1);
+
+% Weed irregular trials out from responses and inputs
+y = r.y(:,2);
+y(r.irr) = [];
+
+u_al = r.u(:,1);
+state = u_al>0.5;
+
+u_al(r.irr) = [];
+state(r.irr) = [];
+
+% Extract trajectories of interest from infStates
+mu1hat = infStates(:,1,1);
+mu2hat = infStates(:,2,1);
+sa1hat = infStates(:,1,2);
+sa2hat = infStates(:,2,2);
+mu3hat = infStates(:,3,1);
+
+
+% move variables from state (contingency space) to response space
+mu2hat_resp = mu2hat;
+mu2hat_resp(state ==0) = -mu2hat(state ==0);
+
+mu1hat_resp = mu1hat;
+mu1hat_resp(state ==0) = 1-mu1hat(state ==0);
+
+sahat1_resp = mu1hat_resp.*(1-mu1hat_resp);
+
+% % Surprise
+% % ~~~~~~~~
+% m1hreg = mu1hat;
+% m1hreg(r.irr) = [];
+% poo = m1hreg.^u.*(1-m1hreg).^(1-u); % probability of observed outcome
+% surp = -log2(poo);
+% surp_shifted = [1; surp(1:(length(surp)-1))];
+
+% Calculate predicted log-reaction time
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+mu1hat_resp(r.irr) = [];
+sahat1_resp(r.irr) = [];
+sa2hat(r.irr) = [];
+mu2hat(r.irr) = [];
+logrt = be0 +be1.*mu2hat_resp + be2.*sa1hat +be3.*sa2hat +be4.*mu3hat;
+
+% Calculate log-probabilities for non-irregular trials
+% Note: 8*atan(1) == 2*pi (this is used to guard against
+% errors resulting from having used pi as a variable).
+reg = ~ismember(1:n,r.irr);
+logp(reg) = -1/2.*log(8*atan(1).*sa) -(y-logrt).^2./(2.*sa);
+yhat(reg) = logrt;
+res(reg) = y-logrt;
+
+return;
---
+++ STE_model_1.m
@@ -0,0 +1,162 @@
+%% TODO
+
+% Try including confidence
+% Try adjust so that we model responses in contingency space rather than
+% correct/incorrect
+
+
+
+
+%%
+clear;
+close all;
+
+addpath('STE_model1');
+
+%% 
+
+% example data (to get contingencies etc)
+sub_data = readtable('STE_data\10369536_A_Threat.csv');
+
+% Contingency space
+sub_data.p_sad = sub_data.Outcome_p_sad/100;
+state = double(sub_data.Cue_idx == sub_data.Outcome_idx);
+u_al = sub_data.p_sad;
+tr_temp = or((state ==1 & sub_data.Outcome_idx== 0), (state == 0 & sub_data.Outcome_idx == 1));
+u_al(tr_temp) = 1 - sub_data.p_sad(tr_temp);
+
+sub_data.u_al = u_al;
+sub_data.state=state;
+
+cue = sub_data.Cue_idx;
+cue(cue==0) = -1; % balanced contrast coding for the "happy bias"
+
+% Responses
+sub_data.logRT = log(sub_data.Response_RT);
+sub_data.correct = sub_data.Outcome_idx == sub_data.Response_idx;
+
+% model input
+u = [sub_data.u_al, cue];
+y = [sub_data.correct, sub_data.logRT];
+
+u_sub = u(~isnan(sub_data.Response_idx),:);
+y_sub = y(~isnan(sub_data.Response_idx),:);
+
+
+%% Get configuration structures
+prc_model_config = prc1_ehgf_binary_pu_tbt_config(); % perceptual model
+obs_model_config = obs1_comb_obs_config(); % response model
+optim_config     = tapas_quasinewton_optim_config(); % optimisation algorithm
+optim_config.nRandInit = 5;
+
+
+%% simulate responses
+
+prc_model_config.logalmu = log(0.1);
+prc_model_config.rhomu(2) = 0;
+prc_model_config.ommu(2)=-2;
+prc_model_config = tapas_align_priors(prc_model_config);
+
+r_temp = [];
+r_temp.c_prc.n_levels = 3;
+prc_params = prc1_ehgf_binary_pu_tbt_transp(r_temp, prc_model_config.priormus);
+
+obs_params = obs_model_config.priormus;
+obs_params(1) = exp(obs_params(1));
+obs_params(7) = exp(obs_params(7));
+
+sim = tapas_simModel(u_sub,...
+    'prc1_ehgf_binary_pu_tbt',...
+    prc_params,...
+    'obs1_comb_obs',...
+    obs_params,...
+    123456789);
+
+
+% Visualise Psychometric
+% y is "correct"
+% sim_sad = (sub_data.Cue_idx == 1 & sim.y(:,1) == 1) + (sub_data.Cue_idx == 0 & sim.y(:,1) == 0);
+sim_sad = (sub_data.p_sad>.5 & sim.y(:,1)==1) + (sub_data.p_sad<.5 & sim.y(:,1)==0);
+
+figure('name', 'simulated psychometric'); hold on;
+sim_psychometric = arrayfun(@(x) mean(sim_sad(sub_data.Outcome_p_sad==x, 1)), 0:20:100);
+plot(0:20:100, sim_psychometric, 'linewidth', 3);
+set(gca, 'Ylim', [0,1], 'Xtick', 0:20:100)
+
+
+
+%% Plot trajectory
+prc1_ehgf_binary_tbt_plotTraj(sim);
+
+
+%% recover parameters
+
+prc_model_config = prc1_ehgf_binary_pu_tbt_config(); % perceptual model
+
+prc_model_config.omsa(2)=3;
+prc_model_config = tapas_align_priors(prc_model_config);
+
+obs_model_config.logzesa=2;
+obs_model_config.beta0sa=2;
+obs_model_config.beta1sa=2;
+obs_model_config.beta2sa=2;
+obs_model_config.beta3sa=2;
+obs_model_config.beta4sa=0;
+obs_model_config.logsasa=2;
+obs_model_config = tapas_align_priors(obs_model_config);
+
+
+est = tapas_fitModel(...
+    sim.y,...
+    sim.u,...
+    prc_model_config,...
+    obs_model_config,...
+    optim_config);
+
+
+%% Simulate psychometric functions
+% config structures
+prc_model_config = prc1_ehgf_binary_pu_tbt_config(); % perceptual model
+obs_model_config = obs1_comb_obs_config(); % response model
+optim_config     = tapas_quasinewton_optim_config(); % optimisation algorithm
+optim_config.nRandInit = 5;
+
+
+figure('name', 'simulated psychometric'); hold on;
+
+for rho = [-5, 0, 5]
+
+    prc_model_config.logalmu = log(.05);
+    prc_model_config.rhomu(2) = rho;
+    prc_model_config.ommu(2)=-2;
+    prc_model_config = tapas_align_priors(prc_model_config);
+
+    r_temp = [];
+    r_temp.c_prc.n_levels = 3;
+    prc_params = prc1_ehgf_binary_pu_tbt_transp(r_temp, prc_model_config.priormus);
+
+    obs_params = obs_model_config.priormus;
+    obs_params(1) = exp(obs_params(1));
+    obs_params(7) = exp(obs_params(7));
+
+    sim = tapas_simModel(u_sub,...
+        'prc1_ehgf_binary_pu_tbt',...
+        prc_params,...
+        'obs1_comb_obs',...
+        obs_params,...
+        123456789);
+
+
+    % Visualise Psychometric
+    % y is "correct"
+    sim_sad = (sub_data.p_sad>.5 & sim.y(:,1)==1) + (sub_data.p_sad<.5 & sim.y(:,1)==0);
+
+    sim_psychometric = arrayfun(@(x) mean(sim_sad(sub_data.Outcome_p_sad==x, 1)), 0:20:100);
+    plot(0:20:100, sim_psychometric, 'linewidth', 3, 'DisplayName', sprintf('\\rho = %1.1f', rho));
+end
+
+set(gca, 'Ylim', [0,1], 'Xtick', 0:20:100)
+ylabel('p(Sad)')
+xlabel('%Sad')
+legend()
+
